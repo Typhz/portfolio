c6168c5b9cc55a2f141f00f1b4d798dd054cf86e
Patrick Reis <patrickbernardor@gmail.com>
2022-02-13 21:21:04 -0300

    [FIX] Navbar bug

--- _layouts/post.js
+++ _layouts/post.js
@@ -1,12 +1,10 @@
 import { Main } from '../styles/article.style';
-import Head from 'next/head'
+import Head from '../src/infra/components/Head'
 
 export default function PostLayout(props){
     return (
         <>
-            <Head>
-                <title>Patrick Reis | {props.title}</title>
-            </Head>
+            <Head title={`Patrick Reis | ${props.title}`}/>
             <Main>
                 <h1>{props.title}</h1>
                 <div dangerouslySetInnerHTML={{__html:props.content}}/>
--- styles/article.style.js
+++ styles/article.style.js
@@ -2,13 +2,9 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   color: #292929;
-  width: 100%;
-  min-height: 200vh;
   padding: 8rem var(--global-padding);
-  flex-wrap: wrap;
   justify-content: center;
   flex-direction: column;
-  flex: 1;
   display: flex;
   h1{
     font-size: 2.4rem;
@@ -18,15 +14,9 @@ export const Main = styled.main`
   }
   h3{
     margin-top: 1.4rem;
-  }
-  small{
-    color: gray;
-    font-weight: 300;
-  }
+  } 
   p{
     padding: 0.5rem 0;
-    font-size: 18px;
-    text-align: justify;
   }
   ul{
     padding: 1rem 1rem;
@@ -44,15 +34,21 @@ export const Main = styled.main`
   }
   code{
     background-color: transparent;
-    font-size: 18px;
     padding: 1rem 1.5rem;
     display: block;
     border-left: 3px solid #292929;
   }
   img{
     width: 100%;
-    margin: 2rem 0;
+    margin: 1rem 0;
   }
   @media only screen and (max-width: 990px) {
+    code{
+      background-color: transparent;
+      padding: 1rem 1.5rem;
+      display: block;
+      border-left: 3px solid #292929;
+      overflow-x: auto;
+    }
   }
 `;
